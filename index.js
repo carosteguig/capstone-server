27f9d7ab5648d325072c1c16ada2b0ff5f008b6b
carosteguig <carosteguig@gmail.com>
2022-03-30 19:34:46 -0700

    created server files

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config(); // load .env variables
+
+const PORT = process.env.PORT || 5050;
