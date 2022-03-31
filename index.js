a92268175cc92ef456b6cf0a8734cf143b1fa9f3
carosteguig <carosteguig@gmail.com>
2022-03-31 12:50:22 -0700

    creating post and get calls

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const favesRecipes = require('./routes/favesRecipesRoute')
 require('dotenv').config(); // load .env variables
 
 const PORT = process.env.PORT || 5050;
@@ -10,8 +11,16 @@ app.use(express.json());  // adds required body (name: req.body.name)
 // app.use(express.static('public'));  // allows to serve the images in public folder.
 app.use(cors()); // allow cross origin resource sharing
 
+// home route
+app.get('/', (req, res) => {
+    res.send('Welcome to the super API');
+});
+
+// // routing
+app.use('/recipe', favesRecipes);
 
 
+app.get('/')
 
 
 app.listen(PORT, () => {
--- routes/favesRecipesRoute.js
+++ routes/favesRecipesRoute.js
@@ -5,7 +5,22 @@ const fs = require('fs');
 
 // Adding a new recipe card to array in favesRecipes.json
 router.post('/', (req,res) => { 
-
+    fs.readFile('./data/favesRecipes.json', 'utf8', (err,data) => {
+        const recipesData = JSON.parse(data);
+        //create new recipe object
+        const newRecipe = {
+            id: req.body.id,
+            title: req.body.title,
+            image: req.body.image,
+            timestamp: new Date(),
+        };
+        // push new recipe into recipeData array
+        recipesData.push(newRecipe);
+        // Write new recipe object data into favesRecipes.json file
+        fs.writeFile('./data/favesRecipes.json', JSON.stringify(recipesData), () => {
+            res.json({message: 'data added to file', data: recipesData});
+        });
+    });
 });
 
 module.exports = router;
