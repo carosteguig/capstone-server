054bdc75632c289afe3d1b9e5d0d65c60ca9b8f1
carosteguig <carosteguig@gmail.com>
2022-03-30 21:05:52 -0700

    created the routes folder and also the data folder with respective files

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ const cors = require('cors');
 require('dotenv').config(); // load .env variables
 
 const PORT = process.env.PORT || 5050;
+
+//Middleware
+app.use(express.json());  // adds required body (name: req.body.name) 
+// app.use(express.static('public'));  // allows to serve the images in public folder.
+app.use(cors()); // allow cross origin resource sharing
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});  
---
+++ routes/favesRecipesRoute.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+// const { v4: uuidv4 } = require('uuid'); PROBABLY NOT NNEDED AS THE RECIPES COME WITH IDs.
+
+// Adding a new recipe card to array in favesRecipes.json
+router.post('/', (req,res) => { 
+
+});
+
+module.exports = router;
